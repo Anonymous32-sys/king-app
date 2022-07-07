c8af837d9cb7286c94a97781d996c637bd3242ee
Anonymous32-sys <arielwobo@gmail.com>
2022-07-07 13:00:15 +0100

    Group Project

--- src/App.js
+++ src/App.js
@@ -1,34 +1,20 @@
 import "./App.css"
 import "./app.scss"
 import "./grid.scss"
-import Navcar from "./component/Navbar";
-import Home from "./component/home";
-import services, { Serve } from "./component/services";
-import Contact from "./component/contact";
-import NavComp from "./component/nav";
-import BoxComp from "./component/box";
-import GridComp from "./component/GridComp";
-import {Routes , Route} from "react-router-dom"
-import ErrorPage from "./component/error";
-import Product from "./component/product";
-
-function App(){
-    return(
-    <div className="App">
-    {/* <Navcar/>
-    <NavComp/> */}
-    <BoxComp/>
-    <div>
-        {/* <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/contact" element={<Contact/>}/>
-            <Route path="/Serve" element={<Serve/>}/>
-            <Route path="*" element={<ErrorPage/>}/>
-            <Route path="/Product" element={<Product/>}/>
-        </Routes> */}
-    </div>
-
-    </div>
+import { Link, Routes } from "react-router-dom"
+import { Head } from "./component/Head"
+import { Route } from "react-router-dom"
+import Routed from "./component/p"
+function App() {
+    return (
+        <div className="App">
+<Head/>
+<div>
+<Routes>
+    
+</Routes>
+</div>
+            </div>
     )
 }
 export default App;
