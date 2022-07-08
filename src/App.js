ecddc9a9541a468a5f3d1561e2c17935d419d496
Anonymous32-sys <arielwobo@gmail.com>
2022-07-08 16:31:25 +0100

    Una dey see

--- src/App.js
+++ src/App.js
@@ -3,16 +3,22 @@ import "./app.scss"
 import "./grid.scss"
 import { Head } from "./component/Head"
 import Slideshow from "./component/test"
+import List from "./component/LIst"
 
 
 
 
 function App() {
-    return (
-        <div className="App">
-      <Head/>
-      <Slideshow/>
-        </div>
-    )
+  return (
+    <>
+      <div className="App">
+        <Head />
+        <Slideshow />
+
+      </div>
+      <div className="Stone">
+        <List />
+      </div>
+    </>)
 }
 export default App;
---
+++ src/component/LIst.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { faDesktop } from "@fortawesome/free-solid-svg-icons";
+const List = () =>{
+    return(
+        <>
+            <div>
+            <h4><e>Hot Sa</e>les</h4>
+            </div>
+        </>
+    )
+}
+export default List
